0a0a1a5a3b3537b8105e57e51ec2c3ad124ab00b
fluffmickgruff <jake.daniel.francis@gmail.com>
2021-02-10 17:36:22 -0500

    particle rotation logic isolated into own function

--- particles.js
+++ particles.js
@@ -120,8 +120,7 @@ class Particle {
     this.dY = dY
     
     this.radius = parRadius
-    this.electronRadius =  this.radius / 5
-    
+    this.electronRadius =  this.radius / 5    
     this.alpha = 0
     this.topSpeed = 0.6 - (Math.random() / 3)
     this.angle = randomFromRange(0,360)
@@ -147,17 +146,24 @@ class Particle {
       this.y += this.dY
   
   }
-  draw(){
+
+  rotate(){
     let radius = this.radius
     let theta = this.angle * (Math.PI/180)
     let cs = Math.cos(theta)
     let sn = Math.sin(theta)
-    let px = (radius * cs) - (radius * sn) + this.x
-    
+    let px = (radius * cs) - (radius * sn) + this.x    
     let py = (radius * sn) + (radius * cs) + this.y
     let a = this.x - px < 0 ? px - this.x: this.x - px
     let b = this.y - py < 0 ? py - this.y: this.y - py
     let d = Math.sqrt((a * a) + (b * b))
+    return {
+     px,py,d
+    }
+  }
+
+  draw(){
+    let rotation = this.rotate()
     ctx.beginPath()
 
     //outer
@@ -175,13 +181,13 @@ class Particle {
 
 
     ctx.beginPath()
-    ctx.arc(px, py,this.electronRadius, 0, 2 * Math.PI, false)
+    ctx.arc(rotation.px, rotation.py,this.electronRadius, 0, 2 * Math.PI, false)
     ctx.strokeStyle = `rgb(0, 250, 204, ${this.alpha / 100})`
     ctx.fill()
     ctx.stroke()
     ctx.closePath()
     ctx.beginPath()
-    ctx.arc(this.x,this.y, d, 0, 2 * Math.PI, false)
+    ctx.arc(this.x,this.y, rotation.d, 0, 2 * Math.PI, false)
     ctx.stroke()
     ctx.closePath()
     this.angle += this.rotation
