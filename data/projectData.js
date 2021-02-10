3eecf63ed2749c1eac7a6f92cef943bfe65f82e1
fluffmickgruff <jake.daniel.francis@gmail.com>
2021-02-10 18:32:41 -0500

    Menu links added to the top of the page

--- data/projectData.js
+++ data/projectData.js
@@ -18,7 +18,11 @@ const projectsData = [
   },
   {
     title: "Paging Alarm Clock",
-    description: `An alarm can be created to send a custom or automated message to an employee's pager. This came about from a need to prevent chemist's from missing time sensitive and legally required samples that monitor pollution.`,
+
+    description: `An alarm can be created to send a custom or automated message 
+    to an employee's pager. This came about from a need to prevent chemists from missing 
+    time sensitive and legally required samples that monitor pollution.`,
+
     live: 'hellojake.com/tetris',
     technologies: ['Javascript','HTML','CSS'],
     code: 'https://github.com/jakefrancis/tetris-rewrite',
